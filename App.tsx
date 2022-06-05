3e4b8850f84e040228e9005501983e0ae1e00208
rapzaDev <rapzadev@gmail.com>
2022-06-05 02:00:30 -0300

    Android navigation bar has been hidden

--- App.tsx
+++ App.tsx
@@ -1,9 +1,9 @@
 import React, { useCallback, useEffect, useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import AppLoading from 'expo-app-loading';
 import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
 import { ThemeProvider } from 'styled-components';
+import * as NavigationBar from 'expo-navigation-bar';
 
 import {
   useFonts,
@@ -16,6 +16,17 @@ import theme from './src/global/styles';
 import AppRoutes from './src/routes/app.routes';
 
 export default function App() {
+
+  const visibility = NavigationBar.useVisibility();
+
+    useEffect(() => {
+        async function setStatusBarVisibility() {
+            await NavigationBar.setBehaviorAsync('overlay-swipe');
+            await NavigationBar.setVisibilityAsync('hidden');
+        } 
+
+        setStatusBarVisibility();
+    },[visibility])
 
   const [ appReady, setAppReady ] = useState(false);
 
